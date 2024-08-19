f3f63cec11959e4fbbe1cf819434b19489687113
SylphsCavort <64819847+SylphsCavort@users.noreply.github.com>
2024-08-19 15:01:02 -0700

    Create addnumbs.py
    Initial commit.

---
+++ addnumbs.py
@@ -0,0 +1,12 @@
+# This is the first version.
+
+num_1 = int(input("First number >"))
+num_2 = int(input("Second number>"))
+
+def adds_numbs(num_1,num_2):
+    result = num_1 + num_2
+    return result
+
+result = adds_numbs(num_1,num_2)
+
+print(result)
